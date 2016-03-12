1d2031c4b95f112069e610c3206c3a439a0a1fcc
UkasikPL <ukasikpl@outlook.pl>
2016-03-12 22:44:57 +0100

    add function

--- mc_server_manager.sh
+++ mc_server_manager.sh
@@ -12,29 +12,38 @@ MAX_RAM="1024"
 A="-Xmx"
 B="M"
 
-function install {
+function reinstall {
 	mkdir $INSTALL_DIR
+	downloadjar
+}
+
+function createdir {
+	mkdir $INSTALL_DIR
+}
+
+function downloadjar {
 	cd $INSTALL_DIR
 	wget "$DW_PATH"
 	cd "../"
 }
 
+function addeula {
+	echo "eula=true" > "$INSTALL_DIR/eula.txt"
+}
 
 # CREATE DIR
 if [ ! -e $INSTALL_DIR ]; then
-	mkdir $INSTALL_DIR
+	createdir
 fi
 
 #IF Craftbukkit.jar not exist download it
 if [ ! -e "$INSTALL_DIR/craftbukkit-1.9.jar" ]; then
-	cd $INSTALL_DIR
-	wget "$DW_PATH"
-	cd "../"
+	downloadjar
 fi
 
 #Create elua and set true
 if [ ! -e "$INSTALL_DIR/elua.txt" ]; then
-	echo "eula=true" > "$INSTALL_DIR/eula.txt"
+	addeula
 fi
 
 #Start server
@@ -42,15 +51,14 @@ if [ $# -eq 1 ] || [ $# -eq 2 ]; then
 	if [ -e "$INSTALL_DIR/craftbukkit-1.9.jar" ] && [ $1 = "start" ]; then
 
 		echo "====================================="
-		echo "Set the amount of RAM: [MB]"
-		read MAX_RAM
+		read -p "Set the amount of RAM [MB]: " MAX_RAM
 		echo "====================================="
 
 		cd $INSTALL_DIR
 		java "$A$MAX_RAM$B" -jar craftbukkit-1.9.jar -o true
 	else if [ $1 = "reinstall" ]; then
 		rm -r $INSTALL_DIR
-		install
+		reinstall
 	fi
 	fi
 fi	
